9ccf6006719a78efba4b8c31f7950c3d42ec65a9
Emmanuelle  Dennemont <emmanuelledennemont@macbook-pro-de-emmanuelle.home>
2022-10-31 12:15:28 +0100

    cloudinary config

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ require("dotenv").config();
 
 const cors = require("cors");
 
+// Import de cloudinary
+const cloudinary = require("cloudinary").v2;
+
 const app = express();
 app.use(cors());
 app.use(formidable());
 
 //mongoose.connect(process.env.MONGODB_URI);
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 app.all("*", (req, res) => {
